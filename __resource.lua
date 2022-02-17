c2b604fdf1beb551a3266e5ce85a453bb1c02205
idlerus <lukas.voborsky@wedos.com>
2022-02-17 15:24:15 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,39 @@
+description '[LoqScript] animated_load - Loading screen made by Idlerus'
+
+files {
+    --Loading Index File--
+    'loading/index.html',
+
+    --Loading Image Files--
+    'loading/image/logo.png',
+    'loading/image/bg1.jpg',
+    'loading/image/bg2.jpg',
+    'loading/image/bg3.jpg',
+    'loading/image/bg4.jpg',
+    'loading/image/bg5.jpg',
+    'loading/image/bg6.jpg',
+    'loading/image/bg7.jpg',
+    'loading/image/bg8.jpg',
+    'loading/image/bg9.jpg',
+
+    --Loading JS Files--
+    --'LoadingScreen/js/materialize.min.js',--
+
+    --Loading CSS Files--
+    'loading/css/animate.min.css',
+
+    --Loading Font Files--
+    'loading/fonts/TitilliumWeb-Black.ttf',
+    'loading/fonts/TitilliumWeb-Bold.ttf',
+    'loading/fonts/TitilliumWeb-BoldItalic.ttf',
+    'loading/fonts/TitilliumWeb-ExtraLight.ttf',
+    'loading/fonts/TitilliumWeb-ExtraLightItalic.ttf',
+    'loading/fonts/TitilliumWeb-Italic.ttf',
+    'loading/fonts/TitilliumWeb-Light.ttf',
+    'loading/fonts/TitilliumWeb-LightItalic.ttf',
+    'loading/fonts/TitilliumWeb-Regular.ttf',
+    'loading/fonts/TitilliumWeb-SemiBold.ttf',
+    'loading/fonts/TitilliumWeb-SemiBoldItalic.ttf',
+}
+
+loadscreen 'LoadingScreen/index.html'
